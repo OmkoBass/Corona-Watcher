19a6ba5a2e5eec70175c6ff1388c475b56b9c93d
OmkoBass <omkob@hotmail.com>
2020-08-07 11:28:30 +0200

    Updated API's.

--- src/Components/Smaller/Footer.js
+++ src/Components/Smaller/Footer.js
@@ -6,7 +6,8 @@ import {Layout} from 'antd';
 function Footer() {
     return <Layout.Footer style={{display: 'flex', flexDirection: 'column', textAlign: 'center'}}>
         @OmkoBass
-        <a href="https://corona.lmao.ninja">API</a>
+        <a href="https://corona.lmao.ninja">CORONA NINJA API</a>
+        <a href="https://documenter.getpostman.com/view/8854915/SzS7R6uu?version=latest">NOVEL COVID API</a>
     </Layout.Footer>
 }
 
