e790baa87af7f459b6ff02b854e77c32d8ce317e
OmkoBass <omkob@hotmail.com>
2020-08-07 16:01:33 +0200

    AutoComplete added.

--- src/Components/Smaller/AllCountires.js
+++ src/Components/Smaller/AllCountires.js
@@ -1,25 +1,30 @@
 import React, {useState, useEffect} from 'react'
 
 //Ant Components
-import {Spin} from "antd";
+import {Spin, AutoComplete} from "antd";
 
 //Components
 import CountryCard from "./CountryCard";
 
 import axios from 'axios'
 
+//React windowing
 import {FixedSizeList} from "react-window";
 import AutoSizer from "react-virtualized-auto-sizer";
 
 function AllCountries() {
     const [allCountries, setAllCountries] = useState(null);
     const [loading, setLoading] = useState(true);
+    const [countryNames, setCountryNames] = useState(null);
 
     useEffect(() => {
         axios.get(`https://corona.lmao.ninja/v2/countries?sort=country`)
             .then(response => {
                 if (response.data)
+                {
                     setAllCountries(response.data);
+                    setCountryNames(response.data.map(country => { return {value: `${country.country}`}}))
+                }
             }).then(() => setLoading(false));
     }, []);
 
@@ -31,12 +36,18 @@ function AllCountries() {
 
     return <div>
         <Spin spinning={loading} tip='Loading...'>
-            <AutoSizer style={{height: '100vh', width: '100%'}}>
+            <AutoComplete
+                style={{width: '100%', display: 'block', margin: 'auto', marginBottom: '1em'}}
+                allowClear
+                placeholder="Country"
+                options={countryNames}
+            />
+            <AutoSizer style={{height: '100vh'}}>
                 {({height, width}) =>
                     <FixedSizeList
                         style={{display: 'block', margin: 'auto'}}
                         itemCount={allCountries?.length}
-                        itemSize={1000}
+                        itemSize={820}
                         height={height}
                         width={width}
                     >
