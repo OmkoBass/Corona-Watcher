10cc4a20fd461afce6a75511dc4cb62cd2502dae
OmkoBass <omkob@hotmail.com>
2021-06-17 20:51:21 +0200

    Moved functions to another file and formatted the file

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 
 //Components
 import Nav from "./Components/Nav";
@@ -9,104 +9,84 @@ import Footer from "./Components/Footer";
 import Loader from "./Components/Loader";
 import GraphedCases from "./Components/GraphedCases";
 
+import {
+  getCountries,
+  getGlobalData,
+  getGlobalHistory,
+} from "./Components/utils";
+
 //css
-import './Styles/style.css'
+import "./Styles/style.css";
 
 //Bootstrap dependency
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-//API
-import { NovelCovid } from 'novelcovid';
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-    const [globalData, setGlobalData] = useState(null);
-    const [countries, setCountries] = useState(null);
-    const [globalHistory, setGlobalHistory] = useState(null);
-    const [toTop, setToTop] = useState(false);
-    const [done, setDone] = useState(false);
+  const [globalData, setGlobalData] = useState(null);
+  const [countries, setCountries] = useState(null);
+  const [globalHistory, setGlobalHistory] = useState(null);
+  const [toTop, setToTop] = useState(false);
+  const [done, setDone] = useState(false);
 
-    async function getGlobalData() {
-        let data = await new NovelCovid();
+  async function setData() {
+    getGlobalData().then((data) => setGlobalData(data));
+    await getCountries().then((data) => setCountries(data));
+    await getGlobalHistory().then((data) => setGlobalHistory(data));
+  }
 
-        return data.all();
+  function scroll() {
+    if (
+      document.body.scrollTop > 460 ||
+      document.documentElement.scrollTop > 460
+    ) {
+      setToTop(true);
+    } else {
+      setToTop(false);
     }
+  }
 
-    async function getCountries() {
-        let data = await new NovelCovid();
+  window.onscroll = () => {
+    scroll();
+  };
 
-        console.log(await data.countries());
+  useEffect(() => {
+    setData().then(function () {
+      setDone(true);
+    });
+  }, []);
 
-        return await data.countries(null, {sort: 'cases'});
-    }
-
-    async function getGlobalHistory() {
-        let data = await new NovelCovid();
-
-        return data.historical(true);
-    }
-
-    async function setData() {
-        await getGlobalData().then(data => setGlobalData(data));
-        await getCountries().then(data => setCountries(data));
-        await getGlobalHistory().then(data => setGlobalHistory(data));
-    }
-
-    function scroll() {
-        if (document.body.scrollTop > 460 || document.documentElement.scrollTop > 460) {
-            setToTop(true);
-        } else {
-            setToTop(false);
-        }
-    }
-
-    window.onscroll = () => {
-        scroll();
-    };
-
-    useEffect(() => {
-
-
-        setData().then(function() {
-            setDone(true);
-        })
-    }, []);
-
-    return (
-        <React.Fragment>
-            <Nav/>
-            <div className='to-top'
-                 style={toTop ? null : {display: 'none'}}
-                 onClick={() => window.scrollTo(0, 0)}>
-                <span>&#8593;</span>
-            </div>
-            {done
-            ?
-                <div>
-                    <CoronaNumbers
-                        update={globalData.updated}
-                        cases={globalData.cases}
-                        deaths={globalData.deaths}
-                        recovered={globalData.recovered}
-                    />
-                    <GraphedCases
-                        cases={globalHistory.cases}
-                        deaths={globalHistory.deaths}
-                        recovered={globalHistory.recovered}
-                    />
-                </div>
-            :
-                <Loader/>
-            }
-            <Info/>
-            {done
-            ?
-                <Countries countries={countries} />
-            :
-                <Loader/>
-            }
-            <Footer/>
-        </React.Fragment>
-    );
+  return (
+    <React.Fragment>
+      <Nav />
+      <div
+        className="to-top"
+        style={toTop ? null : { display: "none" }}
+        onClick={() => window.scrollTo(0, 0)}
+      >
+        <span>&#8593;</span>
+      </div>
+      {done ? (
+        <div>
+          <CoronaNumbers
+            update={globalData.updated}
+            cases={globalData.cases}
+            deaths={globalData.deaths}
+            recovered={globalData.recovered}
+          />
+          <GraphedCases
+            cases={globalHistory.cases}
+            deaths={globalHistory.deaths}
+            recovered={globalHistory.recovered}
+          />
+        </div>
+      ) : (
+        <Loader />
+      )}
+      <Info />
+      {done ? <Countries countries={countries} /> : <Loader />}
+      <Footer />
+    </React.Fragment>
+  );
 }
 
 export default App;
